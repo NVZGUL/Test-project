15f52965716db7399330b8298a7e1b6adf2934b7
Denis Kulikov <altingfest@gmail.com>
2015-11-23 12:47:16 +0300

    initial commit

---
+++ src/js/vendor.js
@@ -0,0 +1,10 @@
+/**************************************/
+/* Vendor JavaScript files supervisor */
+/**************************************/
+
+/* Third party */
+/*
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/bootstrap/dist/js/bootstrap.js
+//= ../../bower_components/fullpage.js/jquery.fullPage.js
+
